341e29a2192ee266c734ee3dcbf92547a06ff0c3
Craig Ryder <craigryder@me.com>
2025-10-06 22:12:07 +0100

    Increase modal width to max-w-6xl to minimize scrolling

--- frontend/src/views/References.vue
+++ frontend/src/views/References.vue
@@ -125,7 +125,7 @@
 
     <!-- Create Reference Modal -->
     <div v-if="showCreateModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
-      <div class="bg-white rounded-lg max-w-4xl w-full mx-4 max-h-[90vh] flex flex-col">
+      <div class="bg-white rounded-lg max-w-6xl w-full mx-4 max-h-[90vh] flex flex-col">
         <div class="p-6 pb-4">
           <h3 class="text-lg font-semibold text-gray-900">Create New Reference</h3>
         </div>
